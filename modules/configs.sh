3baacc11a5de974b338d97309ff6f695312522a2
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2024-07-19 18:23:16 -0400

    fix: wrong dotfile backup dir

--- modules/configs.sh
+++ modules/configs.sh
@@ -3,8 +3,8 @@
 # RESTORE DOTFILES
 echo -e "${ARROW}Restoring dotfiles..."
 cp -R -p -v -i ${BACKUP_DIR}/.config $HOME
-cp -v -i ${HOME}/.zprofile $HOME
-cp -v -i ${HOME}/.zshrc $HOME
+cp -v -i ${BACKUP_DIR}/.zprofile $HOME
+cp -v -i ${BACKUP_DIR}/.zshrc $HOME
 
 # Developer folder for projects
 echo -e "${ARROW}Creating Developer folder..."
