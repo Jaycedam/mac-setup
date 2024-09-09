ec5b27d7b8c09ae37b9fe63a18d7af833b1629a3
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2024-09-09 17:19:48 -0300

    feat: more plist modifications

--- modules/preferences.sh
+++ modules/preferences.sh
@@ -2,35 +2,35 @@
 
 echo -e "${ARROW}Changing macOS defaults..."
 
-# ------------------------------ Dock ------------------------------
-# Autohide
-defaults write com.apple.dock "autohide" -bool "true"
-# Disable recents
-defaults write com.apple.dock "show-recents" -bool "false"
+# Dock
+defaults write com.apple.dock "autohide" -bool true # Autohide
+defaults write com.apple.dock "autohide-delay" -float "0" # Disable delay on mouse over
+defaults write com.apple.dock expose-group-apps -bool true # Groups windows by application on mission control
+defaults write com.apple.dock "mru-spaces" -bool false # Disable rearrange Spaces automatically
+defaults write com.apple.dock "show-recents" -bool false # Disable recents
 
-# ------------------------------ Finder ------------------------------
-# Show file extensions
-defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
-# Show hidden files
-defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
-# Show path bar
-defaults write com.apple.finder "ShowPathbar" -bool "true"
-# List view
-defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
-# Keep folders on top
-defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
-defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" -bool "true"
-# Search scope current folder
-defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf"
+# Finder
+defaults write com.apple.finder "AppleShowAllFiles" -bool true # Show hidden files
+defaults write com.apple.finder "ShowPathbar" -bool true # Show path bar
+defaults write com.apple.finder "ShowStatusBar" -bool true # Show status bar
+defaults write com.apple.finder "FXPreferredViewStyle" -string Nlsv # List view
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool true # Keep folders on top
+defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf" # Search scope current folder
+defaults write com.apple.finder "CreateDesktop" -bool true # Disable desktop icons
+defaults write com.apple.finder "ShowExternalHardDrivesOnDesktop" -bool false # Disable drive icon on desktop
 
-# ------------------------------ Spaces ------------------------------
-# Disable rearrange Spaces automatically
-defaults write com.apple.dock "mru-spaces" -bool "false"
-# Groups windows by application on mission control
-defaults write com.apple.dock expose-group-apps -bool true
 
-# ----------------------------- Security -----------------------------
-# Enable firewall
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+# Security
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on # Enable firewall
 
-killall Dock && killall Finder
+# Screencapture 
+defaults write com.apple.screencapture "location" -string "~/Pictures"
+
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool true # Show file extensions
+
+# Privacy
+defaults write com.apple.AdLib.plist allowApplePersonalizedAdvertising -bool false
+defaults write com.apple.AdLib.plist allowIdentifierForAdvertising -bool false
+defaults write com.apple.AdLib.plist personalizedAdsMigrated -bool false
+
+killall Dock && killall Finder && killall SystemUIServer # Restart dock and finder to apply changes
