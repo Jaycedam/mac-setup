8a4e93ace81fe6ea0af2b33d9e116bf72d77ad5e
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2024-12-17 17:56:18 -0300

    feat(nvim): icon support deps

--- dotfiles/.config/nvim/lua/plugins/fzf.lua
+++ dotfiles/.config/nvim/lua/plugins/fzf.lua
@@ -1,5 +1,7 @@
 return {
 	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		-- calling `setup` is optional for customization
 		-- require("fzf-lua").setup({})
