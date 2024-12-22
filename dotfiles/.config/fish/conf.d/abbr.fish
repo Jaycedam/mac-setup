81396153f691ab3e4fcbbab3afdbdf9ec585240d
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2024-12-22 04:12:18 -0300

    feat: fish config

---
+++ dotfiles/.config/fish/conf.d/abbr.fish
@@ -0,0 +1,15 @@
+# general
+abbr --add so source ~/.config/fish/config.fish
+abbr --add v nvim
+abbr --add vim nvim
+abbr --add ls ls -l
+abbr --add lsa ls -lA
+
+# brew
+abbr --add install brew install
+abbr --add search brew search
+abbr --add upgrade brew upgrade
+abbr --add uninstall brew uninstall --zap
+abbr --add uninstall brew uninstall --zap
+abbr --add outdated brew outdated
+abbr --add autoremove brew autoremove
---
+++ dotfiles/.config/fish/conf.d/general.fish
@@ -0,0 +1,3 @@
+fish_vi_key_bindings # enable vi mode
+
+set -U fish_greeting # disable fish greeting
