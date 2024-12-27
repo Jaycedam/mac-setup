72178240ce1d268aabd3e7d6b9e5fe6fb7eaf55b
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2024-12-27 01:30:18 -0300

    chore: remove fish

--- dotfiles/.config/fish/conf.d/general.fish
+++
@@ -1,6 +0,0 @@
-fish_vi_key_bindings # enable vi mode
-
-set -U fish_greeting # disable fish greeting
-
-# Ctrl-Z suspends a job (like Neovim) and returns to the terminal. Pressing Ctrl-Z again resumes the suspended job. This is an alternative to using a floating terminal in Neovim.
-bind -M insert  'fg'
