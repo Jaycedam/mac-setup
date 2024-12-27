19b955ab0518ebed56829544594bbca554404f70
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2024-12-27 20:43:18 -0300

    feat(nvim): show documentation for completion

--- dotfiles/.config/nvim/lua/plugins/completion.lua
+++ dotfiles/.config/nvim/lua/plugins/completion.lua
@@ -9,6 +9,16 @@ return {
 		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
 		keymap = { preset = "default" },
 
+		completion = {
+			-- Show documentation when selecting a completion item
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+			},
+			-- Display a preview of the selected item on the current line
+			ghost_text = { enabled = true },
+		},
+
 		appearance = {
 			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
 			use_nvim_cmp_as_default = true,
