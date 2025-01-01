7dfc3d9f4cc5bb9c63e23446c8ae7d54e7a1030c
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2024-12-31 23:26:13 -0300

    chore(nvim): change some keymap descriptions

--- dotfiles/.config/nvim/lua/plugins/fzf.lua
+++ dotfiles/.config/nvim/lua/plugins/fzf.lua
@@ -14,17 +14,33 @@ return {
 		})
 
 		-- Keymaps
-		vim.keymap.set("n", "<leader>ff", require("fzf-lua").files, { desc = "[F]ind files" })
-		vim.keymap.set("n", "<leader>fg", require("fzf-lua").live_grep_native, { desc = "[F]ind in project (grep)" })
-		vim.keymap.set("n", "<leader>fb", require("fzf-lua").buffers, { desc = "[F]ind buffers)" })
-		vim.keymap.set("n", "<leader>fk", require("fzf-lua").keymaps, { desc = "[F]ind keymaps" })
-		vim.keymap.set("n", "<leader>fh", require("fzf-lua").helptags, { desc = "[F]ind help" })
+		vim.keymap.set("n", "<leader>ff", require("fzf-lua").files, { desc = "Find files" })
+		vim.keymap.set("n", "<leader>fg", require("fzf-lua").live_grep_native, { desc = "Find in project (grep)" })
+		vim.keymap.set("n", "<leader>fb", require("fzf-lua").buffers, { desc = "Find buffers)" })
+		vim.keymap.set("n", "<leader>fk", require("fzf-lua").keymaps, { desc = "Find keymaps" })
+		vim.keymap.set("n", "<leader>fh", require("fzf-lua").helptags, { desc = "Find help" })
 
 		-- LSP keymaps
-		vim.keymap.set("n", "<leader>gr", require("fzf-lua").lsp_references, { desc = "[G]o to references" })
-		vim.keymap.set("n", "<leader>gd", require("fzf-lua").lsp_definitions, { desc = "[G]o to definitions" })
-		vim.keymap.set("n", "<leader>gi", require("fzf-lua").lsp_implementations, { desc = "[G]o to implementations" })
-		vim.keymap.set("n", "<leader>ge", require("fzf-lua").diagnostics_workspace, { desc = "[G]o to diagnostics)" })
+		vim.keymap.set("n", "<leader>fr", require("fzf-lua").lsp_references, { desc = "Find references (LSP)" })
+		vim.keymap.set("n", "<leader>fd", require("fzf-lua").lsp_definitions, { desc = "Find definitions (LSP)" })
+		vim.keymap.set(
+			"n",
+			"<leader>fi",
+			require("fzf-lua").lsp_implementations,
+			{ desc = "Find implementations (LSP)" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>fe",
+			require("fzf-lua").diagnostics_workspace,
+			{ desc = "Find diagnostics on workspace (LSP)" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>fs",
+			require("fzf-lua").lsp_document_symbols,
+			{ desc = "Find document symbols (LSP)" }
+		)
 		vim.keymap.set("n", "<leader>ca", require("fzf-lua").lsp_code_actions, { desc = "[C]ode actions" })
 	end,
 }
