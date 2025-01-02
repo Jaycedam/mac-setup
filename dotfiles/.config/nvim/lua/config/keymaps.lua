cd700d5cc755f3dad6b6cc38364a25e73d4ccaa2
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-01-02 19:42:31 -0300

    chore(nvim): update keybind descriptions, disable rocks for lazy.nvim

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -3,21 +3,23 @@
 vim.g.mapleader = " "
 local map = vim.keymap.set
 
--- easier split navigation, stolen from tj
-map("n", "<c-j>", "<c-w><c-j>")
-map("n", "<c-k>", "<c-w><c-k>")
-map("n", "<c-l>", "<c-w><c-l>")
-map("n", "<c-h>", "<c-w><c-h>")
+-- easier split navigation
+map("n", "<C-j>", "<c-w><c-j>", { desc = "Move down split " })
+map("n", "<C-k>", "<c-w><c-k>", { desc = "Move up split" })
+map("n", "<C-l>", "<c-w><c-l>", { desc = "Move right split" })
+map("n", "<C-h>", "<c-w><c-h>", { desc = "Move left split" })
 
 -- quickfix list, todo: add toggle to open/close
+map("n", "[q", ":cprev<CR>", { desc = "Quickfix previous" })
 map("n", "]q", ":cnext<CR>", { desc = "Quickfix next" })
-map("n", "[q", ":cprev<CR>", { desc = "Quickfix next" })
 
 map("n", "<leader>e", ":Ex<CR>", { desc = "File Explorer" }) -- vim file explorer
 
 -- buffer navigation
 map("n", "<leader>b", ":buffer #<CR>", { desc = "Go to last buffer" })
+map("n", "[b", ":bprev<CR>", { desc = "Buffer previous" })
+map("n", "]b", ":bnext<CR>", { desc = "Buffer next" })
 
 -- moves selection up/down, without replacing text
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected text down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text up" })
--- dotfiles/.config/nvim/lua/config/lazy.lua
+++ dotfiles/.config/nvim/lua/config/lazy.lua
@@ -31,10 +31,13 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	-- install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = { enabled = true, notify = false },
 
 	defaults = {
 		-- Install the latest stable version of plugins that support Semver.
 		version = "*",
+	},
+	rocks = {
+		enabled = false,
 	},
 })
