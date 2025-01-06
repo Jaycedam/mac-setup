9cc02bcc88a27c0938d4f2b583de30d4ac810de7
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-01-06 19:48:47 -0300

    feat(nvim): zsh formatter, copilot plugin

--- dotfiles/.config/nvim/lua/plugins/formatter.lua
+++ dotfiles/.config/nvim/lua/plugins/formatter.lua
@@ -2,45 +2,38 @@ return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
-	-- This will provide type hinting with LuaLS
-	---@module "conform"
-	---@type conform.setupOpts
-	opts = {
-		-- Define your formatters
-		formatters_by_ft = {
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			typescriptreact = { "prettier" }, -- TSX
-			javascriptreact = { "prettier" }, -- JSX
-			css = { "prettier" },
-			scss = { "prettier" },
-			html = { "prettier" },
-			markdown = { "prettier" },
-			json = { "prettier" },
-			jsonc = { "prettier" },
-			sh = { "shfmt" },
-			lua = { "stylua" },
-			python = { "black" },
-			-- TODO: add the rest of ft
-		},
-		-- Set default options
-		default_format_opts = {
-			lsp_format = "fallback",
-		},
-		-- Set up format-on-save
-		format_on_save = { timeout_ms = 500 },
-		-- Customize formatters
-		formatters = {
-			shfmt = {
-				prepend_args = { "-i", "2" },
+
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				astro = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" }, -- TSX
+				javascriptreact = { "prettier" }, -- JSX
+				css = { "prettier" },
+				scss = { "prettier" },
+				html = { "prettier" },
+				markdown = { "prettier" },
+				json = { "prettier" },
+				jsonc = { "prettier" },
+				sh = { "shfmt" },
+				zsh = { "shfmt" },
+				lua = { "stylua" },
+				python = { "black" },
+				-- TODO: add the rest of ft
 			},
-			prettier = {
-				append_args = { "--tab-width", "4" },
+			format_after_save = {
+				lsp_format = "fallback",
 			},
-		},
-	},
-	init = function()
-		-- If you want the formatexpr, here is the place to set it
-		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+				prettier = {
+					append_args = { "--tab-width", "4" },
+				},
+			},
+		})
 	end,
 }
