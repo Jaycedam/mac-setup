7397c8546fe32a9b84a6bd3781888ce7c31cacb7
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-01-16 20:12:27 -0300

    feat: condition for each os specific scripts

--- main.sh
+++ main.sh
@@ -10,11 +10,13 @@ source modules/brew.sh
 # Restores config files backed up
 source modules/restore.sh
 
-# Changes defaults macOS preferences
-source modules/preferences.sh
-
 # LSP
 source modules/lsp.sh
 
+# Only run this on macOS
+if [ $OS = "Darwin" ]; then
+    # Changes defaults macOS preferences
+    source modules/preferences.sh
+fi
 
 echo -e "${ARROW} Next steps: run ${MAGENTA}source ~/.zshrc'${RESET} to see the shell changes."
