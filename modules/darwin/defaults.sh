d1a5c6d6689aec58f9781e173ffcfe50dd7fe946
Jaycedam <contact@jordancortes.dev>
2025-01-22 01:01:29 -0300

    feat: new defaults override (darwin)

--- modules/darwin/defaults.sh
+++ modules/darwin/defaults.sh
@@ -8,15 +8,25 @@ defaults write com.apple.dock autohide-delay -float 0      # Disable delay on mo
 defaults write com.apple.dock expose-group-apps -bool true # Groups windows by application on mission control
 defaults write com.apple.dock mru-spaces -bool false       # Disable rearrange Spaces automatically
 defaults write com.apple.dock show-recents -bool false     # Disable recents
+defaults write com.apple.dock static-only -bool true       # Only show open apps
+
+# Menubar
+defaults write com.apple.controlcenter BatteryShowPercentage -bool true # Show battery percentage on menubar
+defaults write com.apple.menuextra.clock ShowAMPM -bool false           # hide AM/PM
 
 # Finder
 defaults write com.apple.finder AppleShowAllFiles -bool true                # Show hidden files
+defaults write com.apple.finder AppleShowAllExtensions -bool true           # Show all file extensions
 defaults write com.apple.finder ShowPathbar -bool true                      # Show path bar
 defaults write com.apple.finder ShowStatusBar -bool true                    # Show status bar
-defaults write com.apple.finder FXPreferredViewStyle -string Nlsv           # List view
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv"         # Column view
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"         # Search scope current folder
 defaults write com.apple.finder CreateDesktop -bool true                    # Disable desktop icons
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false # Disable drive icon on desktop
+defaults read com.apple.finder NewWindowTarget -string "PfHm"               # Finder starts on home directory
+defaults write com.apple.finder FXRemoveOldTrashItems -bool true            # Clean trash after 30 days
+defaults write com.apple.finder _FXSortFoldersFirst -bool true              # sort folder first
+defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true     # sort folder first on desktop
 
 # WindowManager
 defaults write com.apple.WindowManager EnableTiledWindowMargins -bool false
@@ -24,7 +34,9 @@ defaults write com.apple.WindowManager EnableTiledWindowMargins -bool false
 # Screencapture
 defaults write com.apple.screencapture location -string "~/Pictures/Screenshots"
 
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true # Show file extensions
+# Screensaver
+defaults write com.apple.screensaver askForPassword -bool true  # Ask for password on screensaver
+defaults write com.apple.screensaver askForPasswordDelay -int 0 # No delay for screensaver password
 
 # Privacy
 defaults write com.apple.AdLib.plist allowApplePersonalizedAdvertising -bool false
@@ -35,8 +47,10 @@ defaults write com.apple.AdLib.plist personalizedAdsMigrated -bool false
 sudo scutil --set HostName jay-macbook
 
 # NSGlobalDomain
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true    # Show file extensions
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false # disables "corrects spelling automatically"
 defaults write -g KeyRepeat -int 2                                 # key repeat rate: fast
 defaults write -g InitialKeyRepeat -int 15                         # delay until repeat: short
+defaults write -g AppleWindowTabbingMode -string "always"          # always prefer tabs
 
 killall Dock && killall Finder && killall SystemUIServer # Restart dock and finder to apply changes
