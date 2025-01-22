b1081ca769517fa5bcbadfed440f501bd7a369c5
Jaycedam <contact@jordancortes.dev>
2025-01-21 21:36:06 -0300

    fix: empty function caused error
    Linux config will be added later

--- main.sh
+++ main.sh
@@ -18,9 +18,6 @@ if [ $OS = "Darwin" ]; then
     # Changes macOS defaults
     source modules/darwin/defaults.sh
     source modules/darwin/enviroment.sh
-# Only run this on Linux
-elif [ $OS = "Linux" ]; then
-    #
 fi
 
-echo -e "${ARROW} Next steps: run ${MAGENTA}source ~/.zshrc'${RESET} to see the shell changes."
+echo -e "${ARROW} Next steps: run ${MAGENTA}source ~/.zshrc${RESET} to see the shell changes."
