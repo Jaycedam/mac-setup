f078a031f328caf0f82dbb22b4113db55c811d74
Jaycedam <contact@jordancortes.dev>
2025-01-25 15:49:26 -0300

    feat: check if brew is installed, fixes #3
    To avoid unnecesary checks

--- modules/brew.sh
+++ modules/brew.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Brew install and setup
-echo -e "${ARROW} Installing Homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Checks if Homebrew is installed
+if command -v brew &>/dev/null; then
+    echo "${YELLOW} Homebrew is already installed. Skipping..."
+else
+    # Brew install and setup
+    echo -e "${ARROW} Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Enables brew in current env
-eval "$(/opt/homebrew/bin/brew shellenv)"
+    # Enables brew in current env
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
 # Disables brew telemetry
 echo -e "${ARROW} Disabling Homebrew telemetry..."
