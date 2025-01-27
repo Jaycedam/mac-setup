c7b6d9f89ffceeb6213e561f8cbef6c31bac106f
Jaycedam <contact@jordancortes.dev>
2025-01-27 11:08:36 -0300

    feat: add checks for fish shell module

--- modules/fish.sh
+++ modules/fish.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# Add fish to known shells
-echo "$(which fish)" | sudo tee -a /etc/shells
+# Check if fish is installed
+if command -v fish >/dev/null 2>&1; then
+    # Path to fish shell
+    fish_path="$(which fish)"
 
-# Change default user shell
-chsh -s "$(which fish)"
+    # Check if fish is already in /etc/shells
+    if ! grep -Fxq "$fish_path" /etc/shells; then
+        # Add fish to known shells
+        echo "$fish_path" | sudo tee -a /etc/shells
+
+        # Change default user shell to fish
+        chsh -s "$fish_path"
+    else
+        echo -e "${YELLOW}Fish shell is already in /etc/shells. Skipping."
+    fi
+else
+    echo -e "${RED}Fish shell is not installed."
+fi
