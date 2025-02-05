bd2bc0e6a30e3ff2d5d5a1ff025dc49d00a5e6d6
Jaycedam <contact@jordancortes.dev>
2025-02-05 19:12:26 -0300

    chore: update configs

--- dotfiles/.config/fish/conf.d/fzf.fish
+++ dotfiles/.config/fish/conf.d/fzf.fish
@@ -4,25 +4,10 @@
 export FZF_DEFAULT_OPTS="
 --bind 'ctrl-y:accept'
 --highlight-line
---height 80% --tmux 80%
+--border
+--height 90% --tmux 80%
 --info=inline-right
 --layout=reverse-list
-
---color=bg+:#2d3f76 
---color=border:#589ed7 
---color=fg:#c8d3f5 
---color=gutter:#1e2030 
---color=header:#ff966c 
---color=hl+:#65bcff 
---color=hl:#65bcff 
---color=info:#545c7e 
---color=marker:#ff007c 
---color=pointer:#ff007c 
---color=prompt:#65bcff 
---color=query:#c8d3f5:regular 
---color=scrollbar:#589ed7 
---color=separator:#ff966c 
---color=spinner:#ff007c 
 "
 
 # Change directory widget, exclude folders
@@ -30,7 +15,7 @@ export FZF_ALT_C_OPTS="
 --walker-skip .git,node_modules,target,Library,Applications,Pictures,Music,.local,.cache,.Trash
 --border-label=' Change Directory '
 --preview-window border-left
---preview 'eza --tree --color=always --icons=always {} | head -200'
+--preview 'eza --tree --level=2 --color=always --icons=always {} | head -198'
 "
 
 # Fzf files widget, exclude folders and preview with bat
--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # If not on tmux, attach or create main session
-    and not set -q TMUX
-    exec tmux new-session -A -s main
+    #and not set -q TMUX
+    #exec tmux new-session -A -s main
 end
