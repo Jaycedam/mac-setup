1988972c9631dabe18de98100b6f89c2ebbedd3a
Jaycedam <contact@jordancortes.dev>
2025-03-31 20:18:16 -0300

    chore(nvim): remove redundant keymaps
    now nvim 0.11 has them by default

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -3,17 +3,5 @@
 vim.g.mapleader = " "
 local map = vim.keymap.set
 
--- quickfix list, todo: add toggle to open/close
-map("n", "[q", ":cprev<CR>", { desc = "Quickfix previous" })
-map("n", "]q", ":cnext<CR>", { desc = "Quickfix next" })
-
--- map("n", "<leader>e", ":Ex<CR>", { desc = "File Explorer" }) -- vim file explorer
-
 -- buffer navigation
 map("n", "<leader>bb", ":buffer #<CR>", { desc = "Go to last buffer" })
-map("n", "[b", ":bprev<CR>", { desc = "Buffer previous" })
-map("n", "]b", ":bnext<CR>", { desc = "Buffer next" })
-
--- moves selection up/down, without replacing text
-map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected text down" })
-map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text up" })
