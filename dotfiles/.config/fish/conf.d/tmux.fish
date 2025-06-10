ab97ed0a246d730be020920e2f08d40042a3c210
Jaycedam <contact@jordancortes.dev>
2025-06-10 01:36:59 -0400

    chore: update dotfiles

--- dotfiles/.config/fish/conf.d/tmux.fish
+++
@@ -1,5 +0,0 @@
-# only run in interactive shells, and when not already inside tmux
-if status --is-interactive; and test -z "$TMUX"
-    #     # -A = attach if exists, else create; -s main names the session “main”
-    tmux new-session -A -s main
-end
