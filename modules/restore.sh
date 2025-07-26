ce9f9e3029f666409b51bb060640fa6c56bffd40
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-07-26 11:23:49 -0400

    feat: manage zsh on main script

--- modules/restore.sh
+++ modules/restore.sh
@@ -3,6 +3,7 @@
 # RESTORE DOTFILES
 echo -e "${ARROW} Creating symlinks to home..."
 ln -svf $BACKUP_DIR/.config $HOME
+ln -svf $BACKUP_DIR/.zshrc $HOME
 
 # This is for bat to see the local themes
 # Remove it if you don't use bat
