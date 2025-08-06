01fce23faf4a2e0c1f5bca881bc08c0d0e602f7a
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-08-06 00:44:02 -0400

    chore: using kitty

--- dotfiles/.config/fish/conf.d/tmux.fish
+++ dotfiles/.config/fish/conf.d/tmux.fish
@@ -1,5 +1,5 @@
-# only run in interactive shells, and when not already inside tmux
-if status --is-interactive; and test -z "$TMUX"
-    #     # -A = attach if exists, else create; -s names the session “main”
-    tmux new-session -A -s main
-end
+# # only run in interactive shells, and when not already inside tmux
+# if status --is-interactive; and test -z "$TMUX"
+#     #     # -A = attach if exists, else create; -s names the session “main”
+#     tmux new-session -A -s main
+# end
